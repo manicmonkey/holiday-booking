662b931bff1c7cadf1892cac943d1b525358cfa1
James Baxter <j.w.baxter@gmail.com>
2021-06-19 22:40:19 +1000

    Add integration test for booking creation

--- src/main/java/consulting/baxter/holidaybooking/rest/BookingController.java
+++ src/main/java/consulting/baxter/holidaybooking/rest/BookingController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -34,17 +36,26 @@ public class BookingController {
 
     @PostMapping("/{propertyName}")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Object> create(@PathVariable String propertyName, @RequestBody Booking booking) {
+    public ResponseEntity<Object> create(@PathVariable String propertyName, @RequestBody Booking booking) throws Exception {
         log.info("Creating booking: {}", booking);
         val maybePropertyEntity = propertyDao.findByName(propertyName);
         //todo grab email from the security context so this can't fail
         val customerEntity = customerDao.findByEmail(booking.getCustomer().getEmail());
 
+        final Function<Booking, URI> toBookingUri = b -> {
+            try {
+                return new URI("/bookingId");
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
         return maybePropertyEntity
             .map(propertyEntity -> new BookingEntity(booking.getDateRange().toEmbeddable(), customerEntity, propertyEntity))
             .map(bookingDao::save)
             .map(Booking::from)
-            .map(ResponseEntity::<Object>ok)
+            .map(toBookingUri)
+            .map(uri -> ResponseEntity.created(uri).build())
             .orElse(ResponseEntity.badRequest().body(Failure.PROPERTY_NOT_FOUND.toString()));
     }
 
--- src/test/java/consulting/baxter/holidaybooking/BookingControllerIntegrationTest.java
+++ src/test/java/consulting/baxter/holidaybooking/BookingControllerIntegrationTest.java
@@ -4,15 +4,20 @@ import consulting.baxter.holidaybooking.rest.model.Booking;
 import consulting.baxter.holidaybooking.rest.model.Customer;
 import consulting.baxter.holidaybooking.rest.model.DateRange;
 import consulting.baxter.holidaybooking.rest.model.Property;
+import lombok.val;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static java.time.temporal.ChronoUnit.WEEKS;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class BookingControllerIntegrationTest {
 
@@ -38,5 +43,34 @@ public class BookingControllerIntegrationTest {
         .then()
             .statusCode(400)
             .body(equalTo("PROPERTY_NOT_FOUND"));
+    }
+
+    @Test
+    void createsBooking() {
+        final Property property = Property.builder().name("batcave").build();
+
+        WebTestClient.bindToServer()
+            .baseUrl("http://localhost:8080").build()
+            .post().uri("/properties")
+            .bodyValue(property)
+            .exchange()
+            .expectStatus().isCreated();
+
+        final Customer customer = Customer.from(InsertDummyData.customer);
+        final DateRange dateRange = DateRange.builder().from(LocalDate.now()).to(LocalDate.now().plus(1, WEEKS)).build();
+        final Booking booking = Booking.builder().property(property).customer(customer).dateRange(dateRange).build();
+        given()
+            .body(booking)
+            .contentType("application/json")
+        .when()
+            .post("/bookings/" + property.getName())
+        .then()
+            .statusCode(201);
+
+        final Booking[] bookings = when()
+            .get("/bookings")
+            .as(Booking[].class);
+
+        Assertions.assertArrayEquals(new Booking[]{booking}, bookings);
     }
 }
